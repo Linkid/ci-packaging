36bff09ea62e64499f19c7b4c03f615762b551c2
François Magimel <magimel.francois@gmail.com>
2017-12-03 16:21:31 +0100

    Remove builds

--- config.sh
+++ config.sh
@@ -5,18 +5,21 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
 
+    :
     #build_zlib
-    build_libpng
-    build_jpeg
+    #build_libpng
+    #build_jpeg
     #build_simple gettext 0.19.8.1 http://ftp.gnu.org/gnu/gettext/ tar.gz
-    build_glib 2.54.2 http://ftp.gnome.org/pub/GNOME/sources/glib/2.54/ tar.xz
-    build_simple pkg-config 0.29.2 http://pkgconfig.freedesktop.org/releases/ tar.gz
-    build_simple libogg 1.3.2 http://downloads.xiph.org/releases/ogg/ tar.gz
-    build_simple libvorbis 1.3.5 http://downloads.xiph.org/releases/vorbis/ tar.gz
-    build_flac 1.3.2 http://downloads.xiph.org/releases/flac/ tar.xz disable-cpplibs
-    build_soundtouch 2.0.0 http://www.surina.net/soundtouch/ tar.gz
-    build_simple SDL 1.2.15 http://www.libsdl.org/release/ tar.gz
-    build_simple SDL_mixer 1.2.12 http://www.libsdl.org/projects/SDL_mixer/release/ tar.gz
+    #build_glib 2.54.2 http://ftp.gnome.org/pub/GNOME/sources/glib/2.54/ tar.xz
+    #build_simple pkg-config 0.29.2 http://pkgconfig.freedesktop.org/releases/ tar.gz
+    #build_simple libogg 1.3.2 http://downloads.xiph.org/releases/ogg/ tar.gz
+    #build_simple libvorbis 1.3.5 http://downloads.xiph.org/releases/vorbis/ tar.gz
+    #build_flac 1.3.2 http://downloads.xiph.org/releases/flac/ tar.xz disable-cpplibs
+    #build_soundtouch 2.0.0 http://www.surina.net/soundtouch/ tar.gz
+    #build_simple SDL 1.2.15 http://www.libsdl.org/release/ tar.gz
+    #build_simple SDL_mixer 1.2.12 http://www.libsdl.org/projects/SDL_mixer/release/ tar.gz
+    #build_simple SDL_ttf 2.0.11 http://www.libsdl.org/projects/SDL_ttf/release/ tar.gz
+    #build_simple SDL_image 1.2.12 http://www.libsdl.org/projects/SDL_image/release/ tar.gz
 }
 
 function build_glib {
