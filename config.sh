adb4bffb7c8fe0a5998edd4e267d15c90ebcf2f4
François Magimel <magimel.francois@gmail.com>
2017-12-03 16:14:40 +0100

    Compile libpng, libjpeg, glib, libogg and libvorbis

--- config.sh
+++ config.sh
@@ -5,16 +5,47 @@ function pre_build {
     # Any stuff that you need to do before you start building the wheels
     # Runs in the root directory of this repository.
 
+    #build_zlib
+    build_libpng
+    build_jpeg
     #build_simple gettext 0.19.8.1 http://ftp.gnu.org/gnu/gettext/ tar.gz
-    #build_glib 2.54.2 http://ftp.gnome.org/pub/GNOME/sources/glib/2.54/ tar.xz
+    build_glib 2.54.2 http://ftp.gnome.org/pub/GNOME/sources/glib/2.54/ tar.xz
     build_simple pkg-config 0.29.2 http://pkgconfig.freedesktop.org/releases/ tar.gz
+    build_simple libogg 1.3.2 http://downloads.xiph.org/releases/ogg/ tar.gz
+    build_simple libvorbis 1.3.5 http://downloads.xiph.org/releases/vorbis/ tar.gz
     build_flac 1.3.2 http://downloads.xiph.org/releases/flac/ tar.xz disable-cpplibs
     build_soundtouch 2.0.0 http://www.surina.net/soundtouch/ tar.gz
     build_simple SDL 1.2.15 http://www.libsdl.org/release/ tar.gz
     build_simple SDL_mixer 1.2.12 http://www.libsdl.org/projects/SDL_mixer/release/ tar.gz
 }
 
+function build_glib {
+    build_xz
+
+    local name="glib"
+    local version=$1
+    local url=$2
+    local ext=${3:-tar.gz}
+    local configure_args=${@:4}
+    if [ -e "${name}-stamp" ]; then
+        return
+    fi
+    local name_version="${name}-${version}"
+    local archive=${name_version}.${ext}
+    fetch_unpack $url/$archive
+    tar Jxf $archive
+    (cd $name_version \
+        && ./configure --prefix=$BUILD_PREFIX $configure_args \
+        && make -C glib \
+        && make -C gthread \
+        && make -C glib install \
+        && make -C gthread install)
+    touch "${name}-stamp"
+}
+
 function build_flac {
+    build_xz
+
     local name="flac"
     local version=$1
     local url=$2
@@ -26,7 +57,7 @@ function build_flac {
     local name_version="${name}-${version}"
     local archive=${name_version}.${ext}
     fetch_unpack $url/$archive
-    tar -Jxf $archive
+    tar  Jxf $archive
     (cd $name_version \
         && sed -i -e 's/AM_PATH_XMMS/true; dnl &/' configure.in \
         && autoreconf -fiv \
