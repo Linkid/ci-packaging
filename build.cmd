8c07c8b7023467cc870a3d8086854bf83bbbb8b0
François Magimel <magimel.francois@gmail.com>
2019-07-11 20:34:08 +0200

    echo on

--- build.cmd
+++ build.cmd
@@ -1,4 +1,4 @@
-@echo off
+:: @echo off
 :: To build extensions for 64 bit Python 3, we need to configure environment
 :: variables to use the MSVC 2010 C++ compilers from GRMSDKX_EN_DVD.iso of:
 :: MS Windows SDK for Windows 7 and .NET Framework 4
